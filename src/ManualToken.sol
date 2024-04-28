6ffc1f246d6235d14a17c82649692f8886c9116f
to4to <justjustifying@outlook.com>
2024-04-29 00:05:59 +0530

    Add transfer function to ManualToken.sol

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -25,4 +25,12 @@ mapping (address => uint) private s_balances;
     function balanceOf(address _owner) public view returns (uint256) {
         return s_balances[_owner];
     }
+
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(s_balances[msg.sender] >= _value);
+        s_balances[msg.sender] -= _value;
+        s_balances[_to] += _value;
+        return true;
+    }
 }
