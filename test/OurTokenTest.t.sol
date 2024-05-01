2210ee11eb5aa2ee425503df520e1f6664baf58e
to4to <justjustifying@outlook.com>
2024-05-01 19:04:04 +0530

    Refactor OurTokenTest contract setup function and fix import statement in OurToken.sol

--- test/OurTokenTest.t.sol
+++ test/OurTokenTest.t.sol
@@ -7,11 +7,10 @@ import {OurToken} from "../src/OurToken.sol";
 contract OurTokenTest is Test {
     OurToken public ourToken;
     DeployOurToken public deployer;
+    uint256 public constant INITIAL_SUPPLY = 100 ether;
 
-
-address bob=makeAddr("bob");
-address alice=makeAddr("alice");
-
+    address bob = makeAddr("bob");
+    address alice = makeAddr("alice");
 
     function setUp() public {
         deployer = new DeployOurToken();
@@ -19,7 +18,23 @@ address alice=makeAddr("alice");
         ourToken = deployer.run();
 
         vm.prank(address(deployer));
-        ourToken.transfer(bob, 1000);
+        ourToken.transfer(bob, INITIAL_SUPPLY);
+    }
 
+    function testBobBalance() public view {
+        assertEq(INITIAL_SUPPLY, ourToken.balanceOf(bob));
+    }
+
+    function testAllowanceWorks() public {
+        uint256 initialAllowance = 1000;
+
+        //Bob Approves alice on her behalf
+        uint256 transferAmount = 100;
+        vm.prank(bob);
+        ourToken.approve(alice, initialAllowance);
+        vm.prank(alice);
+        ourToken.transferFrom(bob, alice, transferAmount);
+
+        ourToken.transfer(bob, transferAmount);
     }
 }
